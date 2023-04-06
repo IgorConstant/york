d3a8932510f64b924a4ef9e63774760c169d3ee1
Igor <igorhenriqueconstant@gmail.com>
2023-04-06 17:18:45 -0300

    Finalização de Model de Segmentos de Produtos

--- application/models/Segmentos_model.php
+++ application/models/Segmentos_model.php
@@ -18,4 +18,27 @@ class Segmentos_model extends CI_Model
             $this->db->insert('app_segmentacoes', $dados);
         }
     }
+
+    public function getsegmentobyID($id = NULL)
+    {
+        if ($id) {
+            $this->db->where('id', $id);
+            $this->db->limit(1);
+            return $this->db->get('app_segmentacoes')->row();
+        }
+    }
+
+    public function atualizarSegmento($dados = NULL, $condicao = NULL)
+    {
+        if (is_array($dados) && is_array($condicao)) {
+            $this->db->update('app_segmentacoes', $dados, $condicao);
+        }
+    }
+
+    public function deletarSegmento($id = NULL)
+    {
+        if ($id) {
+            $this->db->delete('app_segmentacoes', ['id' => $id]);
+        }
+    }
 }
