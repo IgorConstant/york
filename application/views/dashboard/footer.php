1f63a169db314437daaf37d2ba33f5e114527394
Igor <igorhenriqueconstant@gmail.com>
2023-04-06 17:27:56 -0300

    Inclusão de Scripts do Form de upload

--- application/views/dashboard/footer.php
+++ application/views/dashboard/footer.php
@@ -3,6 +3,10 @@
 <script src="<?php echo base_url('assets/dashboard/js/bootstrap.bundle.min.js') ?>"></script>
 <script src="<?php echo base_url('assets/dashboard/js/button.js') ?>"></script>
 <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
+<script src="<?php echo base_url('assets/dashboard/js/upload/jquery.fileuploader.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/dashboard/js/upload/custom.js') ?>"></script>
+
+
 
 <!-- Datatables Claim -->
 <script>
